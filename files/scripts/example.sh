3a66d036dc1955497ce6f7c83987f7ced13939ca
randogoth <kflux@posteo.de>
2025-11-22 14:04:49 +0200

    removed grub stuff

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -4,6 +4,3 @@
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
 set -oue pipefail
-
-sudo chmod 755 /usr/local/sbin/bls-add-bluefin.sh
-sudo systemctl daemon-reload
